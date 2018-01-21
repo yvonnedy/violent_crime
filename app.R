dc219b3f0b06cc90dfd2beae1aedbcda3a36c9f0
yvonnedy <kiiimberly@126.com>
2018-01-21 05:16:25 -0800

    finish the server function

--- app.R
+++ app.R
@@ -21,8 +21,39 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
-
-
+  output$linePlot <- renderPlot({
+    filtered <- dataset %>%
+      filter(department_name %in% input$citiesInput,
+             year >= input$yearInput[1],
+             year <= input$yearInput[2]
+      )
+    ggplot(filtered, aes(x = year, y = violent_per_100k, colour = department_name)) +
+      geom_line() +
+      geom_point() +
+      labs(x = "Year", y = "Violent Crimes Per 100,000 People", colour = "City") +
+      ggtitle("The Rate Of Violent Crime Over Years") +
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5))
+  })
+  
+   output$resultsTable <- renderTable({
+    filtered2 <- dataset %>%
+      filter(department_name %in% input$citiesInput,
+             year >= input$yearInput[1],
+             year <= input$yearInput[2]
+      ) %>% 
+      select(year, department_name, total_pop, violent_per_100k, homs_per_100k, rape_per_100k, rob_per_100k, agg_ass_per_100k) %>% 
+      rename(Year = year,
+             City = department_name,
+             Population = total_pop,
+             Total_crimes_rate = violent_per_100k,
+             Homicides_rate = homs_per_100k,
+             Rapes_rate = rape_per_100k,
+             Robberies_rate = rob_per_100k,
+             Aggravated_assaults_rate = agg_ass_per_100k) 
+    
+    filtered2
+  })
 }
 
 shinyApp(ui = ui, server = server)
