87194805be2a1d416cfc267797b5c34009abc85e
yvonnedy <kiiimberly@126.com>
2018-01-27 03:43:15 -0800

    add functionality of choosing crime type for the line plot

--- app.R
+++ app.R
@@ -9,54 +9,75 @@ library(ggplot2)
 dataset <- read_csv("data/ucr_crime_1975_2015.csv") 
 
 ui <- fluidPage(
+  
   titlePanel("US Violent Crimes Shiny Application"),
+  
   sidebarLayout(
     sidebarPanel(
+      
       selectInput("citiesInput", "City", choices = sort(unique(dataset$department_name)), 
                   selected = "Albuquerque, N.M.", multiple = TRUE),
-      sliderInput("yearInput", "Year Range", min = 1975, max = 2015, step = 1, value = c(1975, 2015))
+      
+      sliderInput("yearInput", "Year Range", min = 1975, max = 2015, step = 1, value = c(1975, 2015)),
+      
+      radioButtons("typeInput", "Crime Type",
+                   choices = c("Total Crime" = "Crimes", "Homicide" = "Homicides", "Rape" = "Rapes", 
+                               "Robbery" = "Robberies", "Aggravated Assault"="Aggravated_Assaults"),
+                   selected = "Crimes")
     ),
+    
     mainPanel(
+      
       plotOutput("linePlot"),
+      
       br(), br(),
+      
       tableOutput("resultsTable")
     )
   )
 )
 
 server <- function(input, output) {
+  
   filtered <- reactive({
     dataset %>%
       filter(department_name %in% input$citiesInput,
              year >= input$yearInput[1],
-             year <= input$yearInput[2]
-      )
-  })  
+             year <= input$yearInput[2]) %>% 
+      rename(Crimes = violent_crime,
+             Homicides = homs_sum,
+             Rapes = rape_sum,
+             Robberies = rob_sum,
+             Aggravated_Assaults = agg_ass_sum)
+  })
   
   output$linePlot <- renderPlot({
-    ggplot(filtered(), aes(x = year, y = violent_crime, colour = department_name)) +
+    
+    ggplot(filtered(), aes_string(x = "year", y = input$typeInput, colour = "department_name")) +
       geom_line() +
       geom_point() +
-      labs(x = "Year", y = "Number Of Violent Crimes ", colour = "City") +
-      ggtitle("The Number Of Violent Crimes Over Years") +
+      labs(x = "Year", y = paste0("The Number Of ", input$typeInput), colour = "City") +
+      ggtitle(paste0("The Number Of Total ", input$typeInput, " Over Years")) +
       theme_bw() +
       theme(plot.title = element_text(hjust = 0.5))
   })
   
-   output$resultsTable <- renderTable({
-    filtered2 <- filtered() %>%
-      select(year, department_name, total_pop, violent_per_100k, homs_per_100k, rape_per_100k, rob_per_100k, agg_ass_per_100k) %>% 
-      rename(Year = year,
-             City = department_name,
-             Population = total_pop,
-             Total_crimes_rate = violent_per_100k,
-             Homicides_rate = homs_per_100k,
-             Rapes_rate = rape_per_100k,
-             Robberies_rate = rob_per_100k,
-             Aggravated_assaults_rate = agg_ass_per_100k) 
-    
-    filtered2
-  })
+  # output$resultsTable <- renderTable({
+  #   
+  #   filtered2 <- filtered() %>%
+  #     select(year, department_name, total_pop, violent_per_100k, homs_per_100k, rape_per_100k, rob_per_100k, agg_ass_per_100k) %>% 
+  #     rename(Year = year,
+  #            City = department_name,
+  #            Population = total_pop,
+  #            Total_crimes_rate = violent_per_100k,
+  #            Homicides_rate = homs_per_100k,
+  #            Rapes_rate = rape_per_100k,
+  #            Robberies_rate = rob_per_100k,
+  #            Aggravated_assaults_rate = agg_ass_per_100k) 
+  #   
+  #   filtered2
+  # })
+
 }
 
 shinyApp(ui = ui, server = server)
